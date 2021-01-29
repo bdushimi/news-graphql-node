4c20106bd0091d3d3cef9bd925e863c4ecb91925
bdushimi <lespharaons@hotmail.com>
2021-01-29 21:10:20 +0200

    Adds filtering capabilites

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,17 @@
-function feed(parent, args, context, info) {
-    return context.prisma.link.findMany();
+async function feed(parent, args, context, info) {
+    const where = args.filter
+        ? {
+            OR: [
+                { description: { contains: args.filter } },
+                {url: { contains: args.filter}},
+            ]
+        
+        }
+        : {}
+    const links = await context.prisma.link.findMany({
+        where
+    });
+    return links;
 }
 
 function getLink(parent, args, context, info) {
